68c6f2836ba7a4fb4dc50ffd8499a3fa4097aae3
Junaid Khan <jk1240651@gmail.com>
2024-05-14 14:38:31 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/Pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/App/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+ content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
